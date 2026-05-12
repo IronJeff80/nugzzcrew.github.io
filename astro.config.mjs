52d8556796721d5c8fc8e01064432c89897be5b8
NugzzCrew <nugzzcrew@gmail.COM>
2026-05-12 13:16:10 -0700

    Update config with site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // Tell Astro your official URL
+  site: 'https://officialnugzzcrew.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
